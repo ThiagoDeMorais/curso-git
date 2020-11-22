b6e5a3714267bd152002d2dc9a29512733881a62
ThiagoDeMorais <demoraisthiago63@gmail.com>
2020-11-22 10:45:17 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ ExemploParaGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExemploParaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
